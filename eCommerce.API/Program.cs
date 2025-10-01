5cac0713e0ad13ea98dc94e4f6d4ed3b7f50dfc2
bhanu <bhanu.r@thinkbridge.in>
2025-10-01 13:02:04 +0530

    basic project set up and DI

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,6 +1,25 @@
+using eCommerce.Infrastructure;
+using eCommerce.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Infra services
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+// add controllers to the service collection
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Routing
+app.UseRouting();
+
+//Auth
+app.UseAuthorization();
+app.UseAuthentication();
+
+//Controller Routes
+app.MapControllers();
 
 app.Run();
---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Extension method to add core to the dpendency injection Container
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            // use: Add sservices to the IoC container
+            // core services often include data access, caching and other low-level components
+
+            return services;
+        }
+    }
+}
